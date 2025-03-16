3f9afde906399c9a828bab2ae405982068f7ad82
Zacharia Ndinguri <zacthuku@gmail.com>
2025-03-16 07:09:29 +0300

    added domcontentloaded

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+  });
+  document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector("p").textContent = "This is really cool!";
+});
